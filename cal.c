7ef5f292d5c86844822ab2d2ad89b912f1bc1c0b
hyeong1 <mi75625@gmail.com>
2022-04-06 19:35:28 +0900

    cal

---
+++ cal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y, sum, sub, mul, div;
+
+    x = 20;
+    y = 10;
+
+    sum = x + y;
+    sub = x - y;
+    mul = x * y;
+    div = x / y;
+
+    printf("두수의 합: %d\n", sum);
+    printf("두수릐 차: %d\n", sub);
+    printf("두수의 곱: %d\n", mul);
+    printf("두수의 몫: %d", div);
+
+    return 0;
+}
