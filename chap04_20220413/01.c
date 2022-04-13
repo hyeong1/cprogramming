4046b8a1b55d791abdebf6d991e3ae9eaacf4404
hyeong1 <mi75625@gmail.com>
2022-04-13 12:45:37 +0900

    file

---
+++ chap04_20220413/01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    //사용자로부터 하나의 실수를 입력받아서 소수점 표기 방법가ㅗ 지수 표기 방법으로 동시 출력
+
+    double number;
+    printf("실수를 입력하시오: ");
+    scanf("%lf", &number);
+
+    printf("실수형식으로는 %lf입니다.\n", number);
+    printf("지수형식으로는 %e입니다.", number);
+
+    return 0;
+}
