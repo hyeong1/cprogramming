8eafc35f7a4d01cb9f2deda29f52781735122b50
hyeong1 <mi75625@gmail.com>
2022-04-16 12:12:44 +0900

    1학기 C기출 22번 17번출력결과

---
+++ 20220416/ex.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // c언어 족보 22번 13번 출력
+    int n = 10;
+    printf("%d\n", !3 == !'\0');
+    printf("%d\n", !3);
+    printf("%d\n", !'\0');
+    return 0;
+}
