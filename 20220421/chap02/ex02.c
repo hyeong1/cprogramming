36c56b5f27ab390756e9738bfd453e9f6e0650b1
hyeong1 <mi75625@gmail.com>
2022-04-21 13:24:18 +0900

    기초C 2장 연습문제 풀이

---
+++ 20220421/chap02/ex02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello\nC\nProgrammer!\n");
+
+    return 0;
+}
---
+++ 20220421/chap02/ex03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("*******************\n");
+    printf("         리포트    \n");
+    printf("*******************\n");
+    printf("\n*******************\n");
+    // printf 한번만 쓰기 -printf("\t학과: 컴퓨터공학과\n\t학번: 20180001\n\t성명: 홍길동\n");
+    printf("\t학과: 컴퓨터공학과\n");
+    printf("\t학번: 20180001\n");
+    printf("\t성명: 홍길동\n");
+    printf("*******************\n");
+
+    return 0;
+}
