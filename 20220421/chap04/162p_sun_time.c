6d054baf64efd1b1cc04a2c7cfe089f1173d5837
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2022-04-26 14:06:51 +0900

    162쪽 2가지 방법

--- 20220421/chap04/162p_sun_time.c
+++ 20220421/chap04/162p_sun_time.c
@@ -9,7 +9,7 @@ int main()
     double time;
     time = distance / light_speed;
     
-    printf("???? ????? %fkm/s \n", light_speed);
+    printf("빛의 속도는 %fkm/s \n", light_speed);
     printf("태양과 지구와의 거리 %fkm \n", distance);
     printf("도달 시간은 %f초 \n", time);
 
@@ -28,10 +28,20 @@ int main()
     // int intTime = time;
     // printf("%d", intTime);
 
-    int min = time / 60;
-    double sec = (int)time % 60;
+    //int min = time / 60;
+    //double sec = (int)time % 60;
 
-    printf("도달 시간은 %d분 %lf초 \n", min, sec);
+    //printf("도달 시간은 %d분 %lf초 \n", min, sec);
+
+
+    // 초를 분으로 바꾼다음에 (초 / 60) 정수형변수에 저장해서 분만 저장하기
+    time = time / 60;
+
+    int minute = time;
+    // 분.XX에서 분 빼고 0.xx에 60을 곱하면 초
+    double second = (time - minute) * 60;
+
+    printf("도달 시간은 %d분 %lf초 \n", minute, second);
 
     return 0;
 }
