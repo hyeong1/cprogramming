f277c92fcce1b5bbaa8c9728e39f202b3a6e566c
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2022-04-27 21:11:58 +0900

    별 찍기-마름모 모양

---
+++ week4_20220427/tree_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 0;i < 5;i++) {
+        for (j = 4;j > i;j--) {
+            printf(" ");
+        }
+        for (j = 0;j < i;j++) {
+            printf("*");
+        }
+        for (j = 0;j < i;j++) {
+            printf("*");
+        }
+        printf("*\n");
+    }
+    for (i = 0;i < 4;i++) {
+        // i == 0, 1, 2, 3 --> 세로로 4번
+        for (j = 0;j <= i;j++) {
+            printf(" ");
+        }
+        for (j = 3;j > i;j--) {
+            printf("*");
+        }
+        for (j = 3;j > i;j--) {
+            printf("*");
+        }
+        printf("*\n");
+    }
+
+    return 0;
+}
