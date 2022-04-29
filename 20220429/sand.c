b683ead1a1f15dc16190d16afac8fb2ad15f1d4d
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2022-04-29 20:51:39 +0900

    별찍기 모래시계 모양

---
+++ 20220429/sand.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 0;i < 5;i++) {
+        for (j = 0;j < i;j++) {
+            printf(" ");
+        }
+        for (j = 4;j > i;j--) {
+            printf("*");
+        }
+        for (j = 4;j > i;j--) {
+            printf("*");
+        }
+        printf("*\n");
+    }
+    for (i = 0;i < 4;i++) {
+        for (j = 3;j > i;j--) {
+            printf(" ");
+        }
+        for (j = 0;j <= i;j++) {
+            printf("*");
+        }
+        for (j = 0;j <= i;j++) {
+            printf("*");
+        }
+        printf("*\n");
+    }
+
+    return 0;
+}
