78a4cbac71a76c743fa529e9a9a031fb16a4a8c9
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2022-05-10 20:28:46 +0900

    계산기-실수입력받으면 실수출력, 정수입력받으면 정수 출력

--- week6_20220510/calculator.c
+++ week6_20220510/calculator.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-double calculator(int select, int num1, int num2) {
+// 입력받은 값이 실수면 실수 출력, 정수면 정수 출력
+// 정수 계산 함수
+int intCalculator(int select, int num1, int num2) {
+    int result;
+    
+    if (select == 1) {
+        result = num1 + num2;
+    }
+    else if (select == 2) {
+        result = num1 - num2;
+    }
+    else if (select == 3) {
+        result = num1 / num2;
+    }
+    else {
+        result = num1 * num2;
+    }
+
+    return result;
+}
+
+//실수 계산 함수
+double doubleCalculator(int select, double num1, double num2) {
     double result;
     
     if (select == 1) {
@@ -29,7 +51,17 @@ int main() {
     printf("연산할 두 수를 입력하세요: ");
     scanf("%lf %lf", &num1, &num2);
 
-    printf("연산 결과: %lf", calculator(select, num1, num2));
+    if (num1 - (int)num1 == 0 && num2 - (int)num2 == 0) {
+        // 정수
+        printf("연산 결과: %d", intCalculator(select, num1, num2));
+    }
+    else if (num1 - (int)num1 != 0 && num2 - (int)num2 == 0) {
+        // 실수
+        printf("연산 결과: %lf", doubleCalculator(select, num1, num2));
+    }
+    else {
+        printf("연산 결과: %lf", doubleCalculator(select, num1, num2));
+    }
 
     return 0;
 }
