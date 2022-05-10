7ee24cf4e8f62ecb7a1af7d0d56a4dc2f80c149c
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2022-05-10 20:51:02 +0900

    랜덤 숫자 찾기-주석추가

--- week6_20220510/ran.c
+++ week6_20220510/ran.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdlib.h> // 난수 발생 헤더파일
+#include <time.h>   // 시드 -> 난수가 계속 바뀌도록
 
 // 1부터 100까지의 수 -맞추기 게임
 // rand() % 100 --> 0부터 99까지
 // 맞추기 기회 5번
 void findNumber() {
-    srand(time(NULL));
+    srand(time(NULL)); // time을 계속 바꿔줘야 실행할 때마다 다른 난수를 발생시킴
 
     int answer;
     answer = rand() % 100 + 1;
