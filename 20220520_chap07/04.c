5f49b590a34be5e3e80b7dcb49ed46fd6cd929b2
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2022-05-20 15:02:48 +0900

    반복문-오른쪽 직각삼각형모양 별찍기

---
+++ 20220520_chap07/04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 0;i < 7;i++) {
+        for (j = 6;j > i;j--) {
+            printf(" ");
+        }
+        for (j = 0;j < i;j++) {
+            printf("*");
+        }
+        printf("*\n");
+    }
+
+    return 0;
+}
