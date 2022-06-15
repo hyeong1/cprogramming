2e31277ccc1d3d8283338c9df8a03d314ce0d68d
MinHyeong Lee <mi75625@gmail.com>
2022-06-15 14:33:14 +0900

    반복문-숫자삼각형 출력

--- loop/num_star.c
+++ loop/num_star.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1;i <= 5;i++){
-        for (int j = i;j <= 5;j++){
-            printf("%d", i);
+    // 1, 12, 123 출력
+    for (int i = 1;i <= 3;i++) {
+        for (int j = 1;j <= i;j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    // 1, 23, 456 출력
+    int num = 1;
+    for (int i = 1;i <= 3;i++) {
+        for (int j = 1;j <= i;j++) {
+            printf("%d", num);
+            num++;
         }
         printf("\n");
     }
