f3205e3ccfe0b3d3e999d51acc88522144f64e4a
MinHyeong Lee <mi75625@gmail.com>
2022-09-09 23:14:52 +0900

    node 구조체

---
+++ data structure/list.c
@@ -0,0 +1,19 @@
+// 단일 열결 리스트 구현
+#include <stdio.h>
+
+typedef struct_node Node;
+struct_node {
+    int data;
+    Node *next;
+}
+
+typedef struct _list {
+    Node *head;
+    Node *tail;
+    int size;
+} List;
+
+int main() {
+
+    return 0;
+}
