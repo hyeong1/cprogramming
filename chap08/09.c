819d59614a48f66b87e5169a9b0c9a6ac9a46766
MinHyeong Lee <mi75625@gmail.com>
2022-09-14 09:08:06 +0900

    고급C교재 8장 함수 연습문제9번

---
+++ chap08/09.c
@@ -0,0 +1,23 @@
+// rand()함수 를 이용하여 0또는 1값을 무작위로 반환하는 함수 b_rand()작성, 5번 호출
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int b_rand();
+
+int main()
+{
+    srand(time(NULL));
+    printf("%d ", b_rand()); 
+    printf("%d ", b_rand()); 
+    printf("%d ", b_rand()); 
+    printf("%d ", b_rand()); 
+    printf("%d ", b_rand()); 
+
+    return 0;
+}
+
+int b_rand()
+{
+    return rand() % 2;
+}
