a0b5a1b4736777317e02406aee5d5e0442d7fff7
hyeong1 <mi75625@gmail.com>
2022-09-19 17:51:24 +0900

    오리구이 문제 주석 추가 17~20번라인

--- test/if_3.c
+++ test/if_3.c
@@ -14,10 +14,10 @@ int main()
 
     if (startMin >= 60) // 종료 분이 60분이 넘을 때
     {
-        startHour += startMin / 60;
-        startMin = startMin % 60;
-        if (startHour >= 24)
-            startHour = startHour % 24;
+        startHour += startMin / 60; // 시간은 시간 + (종료 분 / 60)
+        startMin = startMin % 60;   // 종료 분은 종료 분 나누가 60의 나머지
+        if (startHour >= 24) // 시간이 24시를 넘으면
+            startHour = startHour % 24; // 시간은 시간 나누기 24의 나머지 -> 예) 25시의 경우 25 % 24 == 1시
         printf("%d %d", startHour, startMin);
     }
     else // 종료 분이 60분이 넘지 않으면 바로 출력
