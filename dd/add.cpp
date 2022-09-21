3cedda452133e6ed5a582963e425ade5accf6aab
hyeong1 <mi75625@gmail.com>
2022-09-21 15:09:21 +0900

    컴파일 확인

---
+++ dd/add.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int add(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
---
+++ dd/sub.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int sub(int a, int b)
+{
+    int c;
+    c = a - b;
+    return c;
+}
