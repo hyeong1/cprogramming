393649513e551a4265303c92e94fb7548c088b04
hyeong1 <mi75625@gmail.com>
2022-09-26 21:30:19 +0900

    입력받은 정수와 같거나 큰 소수 중에서 가장 작은 소수 출력하기

--- Baekjoon_p/1747.c
+++ Baekjoon_p/1747.c
@@ -8,13 +8,17 @@
 
 // 입력한 수의 팰린드롬 수 출력하기
 int Palindrome(int n);
+// 입력한 수보다 크거나 같은 소수 중에서 가장 작은 수 찾기
+void Prime(int n);
 
 int main()
 {
     int input, palindrome;
     scanf("%d", &input);
 
-    Palindrome(input);
+    //Palindrome(input);
+    Prime(input);
+
     return 0;
 }
 
@@ -26,5 +30,28 @@ int Palindrome(int n)
     {
         printf("%d", n % 10);
         Palindrome(n / 10);
+    }
+}
+
+void Prime(int n)
+{
+    int i;
+
+    while (1)
+    {
+        for (i = 2;i < n;i++)
+        {
+            if (n % i == 0) // 2 와 n-1 사이에 약수가 있으면 소수 아님 -> n을 하나 증가시키고 중첩반복문을 나감
+            {
+                n++;
+                break;
+            }
+        }
+
+        if (n == i) // 소수 직전 숫자가 n을 하나 증가시키고 중첩반복문 종료 -> 중첩반복문을 전부 돈 n이 입력받은 input보다 크면서 소수이면서 가장 작은 수
+        {
+            printf("%d\n", n);
+            break;
+        }
     }
 }
