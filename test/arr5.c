9d2d6636d95a08c9dbeb165d4da48c6276f6634d
hyeong1 <mi75625@gmail.com>
2022-10-09 11:05:07 +0900

    평균 문제-함수 추가

--- test/arr5.c
+++ test/arr5.c
@@ -4,50 +4,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void InputScore(double *arr, int n);
-
+void InputScore(double *arr, int n); // 입력한 점수를 배열에 넣는 함수
+double Avg(double *arr, int n); // 평균 구하는 함수
+int StudentCount(double *arr, int n, double avg); // 평균 넘는 학생 수 체크하는 함수
+double AvgRate(int n, int count); // 평균 넘는 학생들의 비율을 구하는 함수
 
 int main()
 {
     int c, n, count; // 테스트 케이스 개수, 학생 수, 평균 넘는 학생 체크 변수
     double *score; // 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수
-    double avg, avgRate; // 평균 변수, 평균을 넘는 학생들의 비율 변수
+    double avg; // 평균 변수, 평균을 넘는 학생들의 비율 변수
     scanf("%d\n", &c); // 테스트 케이스 개수 입력
-    avg = 0;
-    count = 0;
-    avgRate = 0;
 
-    for (int i = 0;i < c;i++)
+    for (int i = 0;i < c;i++) // 각 케이스를 c번 반복
     {
         scanf("%d", &n); // 학생 수 입력
         score = (double*)malloc(sizeof(double) * n);
 
         InputScore(score, n); // 점수 입력
-        for (int i = 0;i < n;i++)
-            printf("%lf ", score[i]);
-        printf("\n");
-        // 케이스 마다 평균을 넘는 비율 출력
-        // 평균 구하기
-        for (int i = 0;i < n;i++)
-            avg += score[i];
-        avg = avg / n;
-        printf("평균: %lf\n", avg);
+
+        // 케이스 마다 평균을 넘는 학생 수 비율 출력
+        avg = Avg(score, n); // 평균 구하기
 
         // 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력
-        // 평균 넘는 학생 수 구하기
-        for (int i = 0;i < n;i++) // 점수들을 평균과 비교하면서 
-        {
-            if (score[i] > avg) // 평균보다 크면 count++
-                count++;
-        }
+        count = StudentCount(score, n, avg); // 평균 넘는 학생 수 구하기
+
         // 비율 구하기
-        avgRate = ((double)count / n) * 100; // 비율은 (평균 넘는 학생 수 / 전체 학생수) * 100
-        // 비율 반올림하여 소수점 셋째 자리까지 출력
-        printf("%.3lf\n", avgRate);
-        // 다음 케이스 출력을 위해 평균 변수, 비율 변수, 평균 넘는 학생 수 변수 0으로 초기화히기
-        avg = 0;
-        avgRate = 0;
-        count = 0;
+        printf("%.3lf%%\n", AvgRate(n, count)); // 비율 반올림하여 소수점 셋째 자리까지 출력
     }   
      
     return 0;
@@ -57,4 +40,30 @@ void InputScore(double *arr, int n)
 {
     for (int i = 0;i < n;i++)
         scanf("%lf", &arr[i]);   
+}
+
+double Avg(double *arr, int n)
+{
+    double avg = 0;
+    for (int i = 0;i < n;i++)
+            avg += arr[i];
+    return (avg = avg / n);
+}
+
+int StudentCount(double *arr, int n , double avg) // 전체 학생 수와 평균을 받아서 count 계산
+{
+    int count = 0;
+    for (int i = 0;i < n;i++) // 점수들을 평균과 비교하면서 
+    {
+        if (arr[i] > avg) // 평균보다 크면 count++
+            count++;
+    }
+    return count;
+}
+
+double AvgRate(int n, int count) // 매개변수로 전체 학생수와 평균 넘는 학생수를 받음
+{
+    double avgRate = 0;
+    avgRate = ((double)count / n) * 100;
+    return avgRate;
 }
