e27496328c84475b271658472dd3349d5fbfe7eb
hyeong1 <mi75625@gmail.com>
2022-10-23 00:27:30 +0900

    선택정렬 함수화, 배열 난수로 생성 후 정렬 추가

--- chap10/selectionSort.c
+++ chap10/selectionSort.c
@@ -1,16 +1,45 @@
 // 선택정렬
+// 배열 값 난수로 입력받아서 정렬하기 추가
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void PrintArr(int list[], int size);
+void RandArr(int list[], int size);
+void SelectionSort(int list[], int size);
 
 int main()
 {
-    int list[10] = {5, 6, 7, 8, 9, 1, 2, 10, 3, 4};
+    int list[10];
     int size, temp, least;
     size = 10;
+    srand(time(NULL));
+    RandArr(list, size);
     // 정렬 전 배열 출력하기
-    for (int i = 0;i < size;i++)
-        printf("%d ", list[i]);
+    PrintArr(list, size);
     printf("\n");
     // 선택정렬 수행
+    SelectionSort(list, size);
+    // 정렬된 배열 출력하기
+    PrintArr(list, size);
+    return 0;
+}
+
+void PrintArr(int list[], int size)
+{
+    for (int i = 0;i < size;i++)
+        printf("%d ", list[i]);
+}
+
+void RandArr(int list[], int size)
+{
+    for (int i = 0;i < size;i++)
+        list[i] = rand() % 100;
+}
+
+void SelectionSort(int list[], int size)
+{
+    int least, temp;
     for (int i = 0;i < size-1;i++)
     {
         least = i; // least는 최솟값의 인덱스 -i번째를 최솟값이라 가정
@@ -24,8 +53,4 @@ int main()
         list[i] = list[least]; // i 자리에는 최솟값 요소 저장
         list[least] = temp; // 최솟값이 있었던 자리에는 i번째에 있었던 요소 저장
     }
-    // 정렬된 배열 출력하기
-    for (int i = 0;i < size;i++)
-        printf("%d ", list[i]);
-    return 0;
 }
