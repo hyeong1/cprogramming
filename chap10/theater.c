3b74733ebfcc6da0009e2bd6f588d0e0ffd866c8
hyeong1 <mi75625@gmail.com>
2022-10-26 18:57:18 +0900

    극장 좌석 예약 과제추가

--- chap10/theater.c
+++ chap10/theater.c
@@ -1,52 +1,76 @@
 // 극장 예매 시스템
 // 10월 27일까지 추가할 것
 // 1. 좌석 출력 함수로 바꾸기 o
-// 2. 예매하는 과정 함수로 바꾸기 x
-// 3. 여러 좌석 한 번에 예약받기 x
-// 4. 배열은 2차원으로 바꾸기 x
+// 2. 예매하는 과정 함수로 바꾸기 o
+// 3. 여러명 좌석 한 번에 예약받기 o
+// 4. 배열은 2차원으로 바꾸기 o
 #include <stdio.h>
 
-void PrintSeat(int seat[], int size);
+void PrintSeat(int seat[][10], int rowSize, int colSize); // 좌석 출력 함수
+void ReserveSeat(int seat[][10]);                         // 예매하는 과정 함수
 
 int main()
 {
+    int seat[10][10] = {0};
+    // PrintSeat(seat, 10, 10);
+    ReserveSeat(seat);
+    return 0;
+}
+
+void PrintSeat(int seat[][10], int rowSize, int colSize) // 좌석 출력 함수
+{
+    // 2차원 좌석 출력하기
+    for (int i = 1; i <= 100; i++)
+    {
+        printf("%d\t", i);
+        if (i % 10 == 0)  // i가 10의 배수일 때마다
+            printf("\n"); // 줄바꿈
+    }
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < colSize; j++)
+            printf("%d\t", seat[i][j]);
+        printf("\n");
+    }
+}
+
+void ReserveSeat(int seat[][10]) // 예매하는 과정
+{
     char ans1;
-    int ans2, seat[10] = {0};
+    int member, seatNum;
     while (1)
     {
         printf("좌석을 예약하시겠습니까?(y 또는 n) ");
         scanf(" %c", &ans1);
         if (ans1 == 'y')
         {
-            PrintSeat(seat, 10);
+            PrintSeat(seat, 10, 10);
 
-            printf("몇번째 좌석을 예약하시겠습니까?");
-            scanf("%d", &ans2);
-            if (ans2 <= 0 || ans2 > 10) // 선택한 좌석번호가 0 이하거나 배열 크기인 10 이상이면
+            // 몇 명 예약하시겠습니까 추가
+            printf("몇 명 예약하시겠습니까? ");
+            scanf("%d", &member);            // 예약할 인원 입력받기
+            for (int i = 0; i < member; i++) // 인원 수 만큼 예약과정 반복하기
             {
-                printf("1부터 10사이의 숫자를 입력하세요\n");
-                continue;
+                seatNum = 0; // 좌석 선택 전에 좌석번호 0으로 초기화
+                printf("몇번째 좌석을 예약하시겠습니까? ");
+                scanf("%d", &seatNum);
+                seatNum--;                         // 인덱스로 사용하기 위해 1 감소
+                if (seatNum < 0 || seatNum >= 100) // 선택한 좌석번호가 0 이하거나 배열 크기인 10 이상이면
+                {
+                    printf("1부터 100사이의 숫자를 입력하세요\n");
+                    i--; // i 감소시켜서 다시 예약하도록 만들기
+                    continue;
+                }
+                if (seat[seatNum / 10][seatNum % 10] == 0) // 예약되지 않았으면
+                {
+                    seat[seatNum / 10][seatNum % 10] = 1; // 1로 바꾸고
+                    printf("예약되었습니다.\n");
+                }
+                else // 예약되었으면 (1이면)
+                    printf("이미 예약된 자리입니다.\n");
             }
-            if (seat[ans2 - 1] == 0) // 예약되지 않았으면
-            {
-                seat[ans2 - 1] = 1; // 1로 바꾸고
-                printf("예약되었습니다.\n");
-            }
-            else // 예약되었으면 (1이면)
-                printf("이미 예약된 자리입니다.\n");
         }
         else if (ans1 == 'n')
-            return 0; // 대답이 n이면 종료
+            break; // 대답이 n이면 종료
     }
-    return 0;
-}
-
-void PrintSeat(int seat[], int size)
-{
-    printf("------------------------\n");
-    printf("1 2 3 4 5 6 7 8 9 10\n");
-    printf("------------------------\n");
-    for (int i = 0; i < size; i++)
-        printf("%d ", seat[i]); // 좌석 예매 유무 출력 -0이면 예약x, 1이면 예약o
-    printf("\n");
 }
