926f632ba2b1a2a5792580dbba6c18aa978f6d41
hyeong1 <mi75625@gmail.com>
2022-10-27 14:12:54 +0900

    자리수 출력 수정

--- chap09/printDigit.c
+++ chap09/printDigit.c
@@ -1,17 +1,15 @@
 // 순환 호출로 각 자리수 출력하기
 #include <stdio.h>
 
-void PrintDigit(int n)
+int PrintDigit(int n)
 {
-    if (n > 0)
-    {
+    if (n > 10)
         PrintDigit(n / 10);
-        printf("%d\n", n % 10);
-    }
+    printf("%d\n", n % 10);
 }
 
 int main()
 {
-    PrintDigit(234);
+    PrintDigit(1234);
     return 0;
 }
