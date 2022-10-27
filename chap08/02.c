c1c7562d5dc942519d5446be487e902bff07ef83
hyeong1 <mi75625@gmail.com>
2022-10-27 14:12:37 +0900

    알파벳 문자인지 확인 수정

--- chap08/02.c
+++ chap08/02.c
@@ -19,18 +19,22 @@ void check_alpha(char ch)
     if (ch >= 97 || ch < 123)
         printf("%c는 알파벳 문자입니다.\n", ch);
 }*/
-void CheckAlpha(char ch)
+int CheckAlpha(char ch)
 {
-    if (ch >= 'a' && ch <= 'z')
+    /*if (ch >= 'a' && ch <= 'z')
         printf("%c는 알파벳 문자입니다.\n", ch);
     else
-        printf("%c는 알파벳 문자가 아닙니다.\n", ch);
+        printf("%c는 알파벳 문자가 아닙니다.\n", ch);*/
+    return (ch >= 'a' && ch <= 'z');
 }
 
 int main()
 {
     char ch;
     scanf(" %c", &ch);
-    CheckAlpha(ch);
+    if (CheckAlpha(ch))
+        printf("%c는 알파벳 문자입니다.\n", ch);
+    else
+        printf("%c는 알파벳 문자가 아닙니다.\n", ch);
     return 0;
 }
