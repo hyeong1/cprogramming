4dbe8d386ee4637460789f253f3fa5cc944190a5
hyeong1 <mi75625@gmail.com>
2022-11-06 23:18:48 +0900

    손익분기점 문제 - 반복문 없이 풀기

---
+++ Baekjoon_p/1712.c
@@ -0,0 +1,15 @@
+// 손익분기점 구하는 문제
+#include <stdio.h>
+
+int main()
+{
+    int fix, variable, price;
+    scanf("%d %d %d", &fix, &variable, &price);
+    if (variable >= price)
+    {
+        printf("%d", -1);
+        return 0;
+    }
+    printf("%d", (fix)/(price-variable) + 1);
+    return 0;
+}
