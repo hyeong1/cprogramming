8f0ca58a221ff041fd87ea4fc27b5d8e8ffd683d
hyeong1 <mi75625@gmail.com>
2022-11-08 19:19:02 +0900

    동적할당 메모리 반납-free

--- Baekjoon_p/n_x.c
+++ Baekjoon_p/n_x.c
@@ -8,14 +8,16 @@ int main()
     int n, x;
     int *num;
     scanf("%d %d", &n, &x);
-    num = (int*)malloc(sizeof(int) * n);
+    num = (int *)malloc(sizeof(int) * n);
 
-    for (int i = 0;i < n;i++)
+    for (int i = 0; i < n; i++)
         scanf("%d", &num[i]);
-    for (int i = 0;i < n;i++)
+    for (int i = 0; i < n; i++)
     {
         if (num[i] < x)
             printf("%d ", num[i]);
     }
+
+    free(num); // 동적할당 메모리 반납
     return 0;
 }
