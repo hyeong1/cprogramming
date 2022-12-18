2781d79730e92ed8cb796f797a0050d7f54fd0a5
hyeong1 <mi75625@gmail.com>
2022-12-18 14:33:24 +0900

    구조체 함수, 구조체 포인터 사용

---
+++ chap13/struct_fun.c
@@ -0,0 +1,26 @@
+// 구조체 함수, 포인터 사용
+#include <stdio.h>
+
+struct point
+{
+    int x, y;
+};
+
+int equal(struct point *pp1, struct point *pp2);
+
+int main()
+{
+    struct point p1 = {1, 2};
+    struct point p2 = {1, 2};
+
+    printf("일치 여부 = %d\n", equal(&p1, &p2));
+
+    return 0;
+}
+
+int equal(struct point *pp1, struct point *pp2)
+{
+    if(pp1->x == pp2->x && pp1->y == pp2->y)
+        return 1;
+    return 0;
+}
