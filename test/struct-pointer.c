5667ad919049373c71935edf3b4a4e6ceee20ee3
hyeong1 <mi75625@gmail.com>
2022-12-18 15:17:12 +0900

    구조체 배열 동적 할당 후 메모리 반납 free 추가

--- test/struct-pointer.c
+++ test/struct-pointer.c
@@ -17,10 +17,10 @@ int main()
     int best; // 학점이 가장 높은 학생 찾을 때 사용할 변수
     printf("학생 수: ");
     scanf("%d", &size);
-    list = (struct student*)malloc(size * sizeof(struct student));
+    list = (struct student *)malloc(size * sizeof(struct student));
 
     // 정보 입력
-    for (int i = 0;i < size;i++)
+    for (int i = 0; i < size; i++)
     {
         printf("학번을 입력하시오: ");
         scanf("%d", &list[i].number);
@@ -30,15 +30,17 @@ int main()
         scanf("%lf", &list[i].grade);
     }
     // 전체 정보 출력
-    for (int i = 0;i < size;i++)
+    for (int i = 0; i < size; i++)
         printf("학번: %d, 이름: %s,학점: %lf\n", list[i].number, list[i].name, list[i].grade);
 
     // 학점이 가장 높은 학생의 정보 출력
-    best = 0;  
-    for (int i = 1;i < size;i++)
+    best = 0;
+    for (int i = 1; i < size; i++)
         if (list[best].grade < list[i].grade)
             best = i; // 학점이 가장 높은 학생의 인덱스를 저장
     printf("학점이 가장 높은 학생은 (학번: %d, 이름: %s, 평점: %lf)입니다.\n", list[best].number, list[best].name, list[best].grade);
+
+    free(list); // 동적 할당 마지막에 메모리 반납
 
     return 0;
 }
