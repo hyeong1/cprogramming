6f8ce144f715d1cd9c1a882bf1face14f9a8384a
hyeong1 <mi75625@gmail.com>
2022-12-20 07:25:49 +0900

    극장예약 해야할 것 정리

--- chap10/theater.c
+++ chap10/theater.c
@@ -1,13 +1,30 @@
-// 극장 예매 시스템
-// 시작화면으로 추가할 것
-// 1. 로그인 -> 선택 시 id, password 입력 - 맞으면 예약 진행/틀리면 다시 id, password 입력받게 해라 (함수) 나중에 로그인해서 내 예약정보까지 확인할 수 있게 할거임
-// 2. 회원가입 -> 회원정보는 id, password 받아서 구조체 배열로 저장 (함수)
-// 3. 종료 -> 선택 시 끝 (함수)
+/*
+극장 예매 시스템 (과제 마감: 12월 24일 24:00까지 이메일로 제출-소스파일만(.c파일만))
+시작화면으로 추가할 것
+1. 로그인 -> 선택 시 id, password 입력 - 맞으면 예약 진행/틀리면 다시 id, password 입력받게 해라 (함수) 나중에 로그인해서 내 예약정보까지 확인할 수 있게 할거임
+  - 로그인 시 파일에서 정보 읽어와서 수행
+2. 회원가입 -> 회원정보는 id, password 받아서 구조체 배열로 저장 (함수) -> 회원은 3명
+  - 회원가입 시 회원 정보 파일로 저장 (메모리 동적할당해서 회원가입하면 가산점)
+3. 종료 -> 선택 시 끝 (함수)
+회원가입될 때마다 메모리 할당해서 저장을 해주면? -회원이 늘어날 때마다 메모리 할당해주기
+-인덱스 쓰지말고 모두 포인터로 사용
+
+아직 못한 것들
+-파일 & 배열 회원 수 제한
+-저장된 파일에서 id먼저 가져와서 입력된 값이랑 비교하고
+-해당 id의 비밀번호까지 비교해서 로그인 수행하기
+-인덱스 모두 포인터로 바꾸기
+-시작할 때마다 전 실행에서 입력한 회원 정보들 기억
+생각
+1. 프로그램이 실행되면 파일에 입력된 모든 정보를 main의 구조체 배열로 가져옴
+2. main으로 가져와서 회원 수 제한 - 회원가입하려고 하는데 제한 인원 넘으면 x
+3. 회원가입을 선택할 때마다 동적 메모리 할당하면 가산점
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct member // 사용자 회원정보 저장
+typedef struct member // 사용자 회원정보 저장 -회원 3명??
 {
     char id[10];
     char password[50];
@@ -15,14 +32,17 @@ typedef struct member // 사용자 회원정보 저장
 
 void PrintSeat(int seat[][10], int rowSize, int colSize); // 좌석 출력 함수
 void ReserveSeat(int seat[][10]);                         // 예매하는 과정 함수
-JOIN Login(JOIN *userList, int seat[][10]);               // 로그인하는 함수
-JOIN JoinMember(JOIN *userList);                          // 회원가입하는 함수
+void Login(JOIN *userList, int seat[][10]);               // 로그인하는 함수
+void JoinMember(JOIN *userList, FILE *member);            // 회원가입하는 함수
 
 int main()
 {
-    int seat[10][10] = {0};   // 좌석 - 2차원 배열 생성
-    int userChoice;           // 시작화면에서 1, 2, 3 선택 결과 저장하는 변수
-    JOIN userInformation[10]; // 회원가입 인원은 일단 10명으로 설정 - main에 설정해서 다른 함수에서 사용할 수 있게 하기
+    int seat[10][10] = {0};                        // 좌석 - 2차원 배열 생성
+    int userChoice;                                // 시작화면에서 1, 2, 3 선택 결과 저장하는 변수
+    JOIN userInformation[10];                      // 회원가입 인원은 3명
+    FILE *fMembers = fopen("membership.txt", "a"); // "a"는 파일 뒤에 덧붙이기
+    // 프로그램 실행되면 main에서 구조체 배열에 파일 정보들 가져오기
+
     /*
     // 회원가입 함수 테스트
     JoinMember(userInformation);
@@ -50,7 +70,7 @@ int main()
             Login(userInformation, seat);
             break;
         case 2: // 2 선택하면 회원가입 함수 실행
-            JoinMember(userInformation);
+            JoinMember(userInformation, fMembers);
             break;
         case 3: // 3 선택하면 강제종료
             exit(0);
@@ -58,7 +78,7 @@ int main()
             break;
         }
     }
-
+    fclose(fMembers);
     // ReserveSeat(seat); // 예약하기
     return 0;
 }
@@ -124,7 +144,7 @@ void ReserveSeat(int seat[][10]) // 예매하는 과정
     }
 }
 
-JOIN Login(JOIN *userList, int seat[][10]) // 로그인하는 함수 - id, password 입력받아서 비교하기
+void Login(JOIN *userList, int seat[][10]) // 로그인하는 함수 - id, password 입력받아서 비교하기
 {
     char inputId[10], inputPassword[50]; // 사용자가 id, password 입력하는 문자열
     // id, password 입력받기
@@ -159,14 +179,17 @@ JOIN Login(JOIN *userList, int seat[][10]) // 로그인하는 함수 - id, password 입�
     }
 }
 
-JOIN JoinMember(JOIN *userList) // 회원가입하는 함수 - member 구조체 배열을 매개변수로 받음
+void JoinMember(JOIN *userList, FILE *member) // 회원가입하는 함수 - member 구조체 배열을 매개변수로 받음
 {
-    static int userNum = 0;      // userInformation 구조체 배열의 인덱스로 사용할 변수
-    printf("%d번째\n", userNum); // static 변수 확인용
+    static int userNum = 0;      // userInformation 구조체 배열의 인덱스로 사용할 변수 -> 실행될 때마다 인덱스가 원하는대로 증가하지 않고 0부터 시작함 -> static 변수로 바꿈
+    printf("%d번째\n", userNum); // 인덱스 변수 확인용
     printf("사용할 id를 입력하세요: ");
     scanf("%s", userList[userNum].id);
+    // fscanf(fp, "%", &변수); --> 파일에 입력하기
+
     printf("사용할 password를 입력하세요: ");
     scanf("%s", userList[userNum].password);
+    fprintf(member, "%s\t%s\n", userList[userNum].id, userList[userNum].password);
 
     userNum++; // 회원가입하고 userNum 증가시키고 함수 종료
 }
