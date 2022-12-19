48fd70a0ce18d05f3c2ef65938caac506ce4ffc5
hyeong1 <mi75625@gmail.com>
2022-12-20 05:54:35 +0900

    2차원 배열 크기 - 전역변수로 변경

--- chap14/pp8.c
+++ chap14/pp8.c
@@ -1,19 +1,21 @@
 // 크기가 3*3인 2차원 배열을 다른 2차원 배열로 복사하는 함수 void array_copy(int src[][WIDTH], int dist[][WIDTH]) 구현
 // 배열 첨자 방법 대신 포인터 사용
 #include <stdio.h>
+#define WIDTH 3
+#define HEIGHT 3
 
-void array_copy(int src[][3], int dist[][3]);
-void print_array(int arr[][3]);
+void array_copy(int src[][WIDTH], int dist[][WIDTH]);
+void print_array(int arr[][WIDTH]);
 
 int main()
 {
-    int original[3][3] = {{100, 200, 300},
-                          {10, 20, 30},
-                          (1, 2, 3)};
-    int copyArr[3][3] = {0}; // 복사할 배열
+    int original[HEIGHT][WIDTH] = {{100, 200, 300},
+                                   {10, 20, 30},
+                                   (1, 2, 3)};
+    int copyArr[HEIGHT][WIDTH] = {0}; // 복사할 배열
 
     array_copy(original, copyArr);
-    
+
     printf("원본 배열\n");
     print_array(original);
     printf("복사 배열\n");
@@ -22,21 +24,21 @@ int main()
     return 0;
 }
 
-void array_copy(int src[][3], int dist[][3])
+void array_copy(int src[][WIDTH], int dist[][WIDTH])
 {
-    for (int i = 0;i < 3;i++) // 행
+    for (int i = 0; i < HEIGHT; i++) // 행
     {
-        for (int j = 0;j < 3;j++) // 열
-            *(*(dist+i)+j) = *(*(src+i)+j); // 안쪽 괄호(i)가 행 증가, 밖 괄호(j)가 열 증가
+        for (int j = 0; j < WIDTH; j++)             // 열
+            *(*(dist + i) + j) = *(*(src + i) + j); // 안쪽 괄호(i)가 행 증가, 밖 괄호(j)가 열 증가
     }
 }
 
-void print_array(int arr[][3])
+void print_array(int arr[][WIDTH])
 {
-    for (int i = 0;i < 3;i++)
+    for (int i = 0; i < HEIGHT; i++)
     {
-        for (int j = 0;j < 3;j++)
-            printf("%d ", *(*(arr+i)+j));
+        for (int j = 0; j < WIDTH; j++)
+            printf("%d ", *(*(arr + i) + j));
         printf("\n");
     }
 }
