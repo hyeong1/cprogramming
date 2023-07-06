cc1f133f044ec43d4e625df235bddf96673f6754
hyeong1 <mi75625@gmail.com>
2023-07-06 16:54:35 +0900

    2차원 배열

--- Baekjoon_p/2563.c
+++ Baekjoon_p/2563.c
@@ -1,11 +1,27 @@
-// 10*10 크기의 색종이
-// 100*100 크기의 도화지에 올리기
-// 색종이의 총 면적 출력
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
+    int paper[100][100] = {0};
+    int test_case, total = 0;
+    scanf("%d", &test_case);
 
+    for (int i = 0; i < test_case; i++)
+    {
+        int row, col;
+        scanf("%d %d", &row, &col);
+        for (int j = 0; j < 10; j++)
+            for (int k = 0; k < 10; k++)
+                paper[row + j][col + k] = 1;
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 100; j++)
+            if (paper[i][j] == 1)
+                total++;
+    }
+
+    printf("%d", total);
     return 0;
 }
